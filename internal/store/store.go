9d9f68d02267e3aea15216af36054c345fdaad74
YxTiBlya <karasevstepan674@gmail.com>
2023-04-25 06:23:37 +0400

    updated migrate models

--- internal/store/store.go
+++ internal/store/store.go
@@ -14,13 +14,14 @@ var DB *gorm.DB
 func ConnToDB(c *cfg.Config) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("user=%v password=%v host=%v port=%v dbname=%v sslmode=%v", c.Postgres_username, c.Postgres_password, c.Postgres_host, c.Postgres_port, c.Postgres_dbname, c.Postgres_ssl)
 
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 
 	DB.AutoMigrate(
-		&models.Restaurant{},
+		&models.Products{},
 	)
 
 	return DB, nil
