6a553a8a6f9384465c8d7bdd5c5c6d0e8402e32b
YxTiBlya <karasevstepan674@gmail.com>
2023-05-21 04:26:00 +0400

    converter fix

--- internal/timestamp/converter.go
+++ internal/timestamp/converter.go
@@ -6,6 +6,17 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func ToTimestamppb(time time.Time) *timestamppb.Timestamp {
-	return &timestamppb.Timestamp{Seconds: time.Unix()}
+func ToTimestamppb(tm time.Time) *timestamppb.Timestamp {
+	converted_time := time.Date(
+		tm.Year(),
+		tm.Month(),
+		tm.Day(),
+		tm.Hour(),
+		tm.Minute(),
+		tm.Second(),
+		tm.Nanosecond(),
+		time.Local,
+	)
+
+	return &timestamppb.Timestamp{Seconds: converted_time.Unix()}
 }
