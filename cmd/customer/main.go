ec589001d988e0242babcca10d70a7a769302714
YxTiBlya <karasevstepan674@gmail.com>
2023-05-22 00:43:28 +0400

    inits and connect moved to run.go

--- cmd/customer/main.go
+++ cmd/customer/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/BurntSushi/toml"
 	"github.com/MSFT/internal/cfg"
 	"github.com/MSFT/internal/gateway"
 	gateway_customer "github.com/MSFT/internal/gateway/customer"
-	"github.com/MSFT/internal/rabbitmq"
-	"github.com/MSFT/internal/store"
 )
 
 func main() {
@@ -20,28 +15,8 @@ func main() {
 	}
 	cfg.UpdateConfig(config)
 
-	// create the connection to db
-	if _, err := store.ConnToDB(config); err != nil {
-		panic("failed to connect database:\n" + err.Error())
-	}
-
-	// create the connection to rabbitmq
-	if err := rabbitmq.ConnToRabbitMQ(config); err != nil {
-		panic("failed to connect rabbitmq:\n" + err.Error())
-	}
-
-	// logger init
-	if config.Logging_in_file {
-		logger_file, err := os.OpenFile("logger/customer.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			panic("failed to create or opening the logger file:\n" + err.Error())
-		}
-		defer logger_file.Close()
-		log.SetOutput(logger_file)
-	}
-
 	// serve
 	if err := gateway.Run(config, &gateway_customer.CustomerServer{}, false); err != nil {
-		log.Fatal("error running gateway server ", err)
+		panic("error running gateway server:\n" + err.Error())
 	}
 }
--- cmd/restaurant/main.go
+++ cmd/restaurant/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/BurntSushi/toml"
 	"github.com/MSFT/internal/cfg"
 	"github.com/MSFT/internal/gateway"
 	gateway_restaurant "github.com/MSFT/internal/gateway/restaurant"
-	"github.com/MSFT/internal/rabbitmq"
-	"github.com/MSFT/internal/store"
 )
 
 func main() {
@@ -20,28 +15,8 @@ func main() {
 	}
 	cfg.UpdateConfig(config)
 
-	// create the connection to db
-	if _, err := store.ConnToDB(config); err != nil {
-		panic("failed to connect database:\n" + err.Error())
-	}
-
-	// create the connection to rabbitmq
-	if err := rabbitmq.ConnToRabbitMQ(config); err != nil {
-		panic("failed to connect rabbitmq:\n" + err.Error())
-	}
-
-	// logger init
-	if config.Logging_in_file {
-		logger_file, err := os.OpenFile("logger/restaurant.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			panic("failed to create or opening the logger file:\n" + err.Error())
-		}
-		defer logger_file.Close()
-		log.SetOutput(logger_file)
-	}
-
 	// serve
 	if err := gateway.Run(config, &gateway_restaurant.RestaurantServer{}, true); err != nil {
-		log.Fatal("error running gateway server ", err)
+		panic("error running gateway server:\n" + err.Error())
 	}
 }
