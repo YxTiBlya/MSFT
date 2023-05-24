57edd75d660c0168923b284fb592ee3fa4fc5d4e
YxTiBlya <karasevstepan674@gmail.com>
2023-05-24 06:36:22 +0400

    added statistics service

---
+++ cmd/statistics/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/MSFT/internal/cfg"
+	"github.com/MSFT/internal/gateway"
+	gateway_statistics "github.com/MSFT/internal/gateway/statistics"
+)
+
+func main() {
+	// parse and update config
+	config := cfg.GetConfig()
+	if _, err := toml.DecodeFile("config.toml", config); err != nil {
+		panic("failed to decode toml file:\n" + err.Error())
+	}
+	cfg.UpdateConfig(config)
+
+	// serve
+	if err := gateway.Run(config, &gateway_statistics.StatisticsServer{}, true); err != nil {
+		panic("error running gateway server:\n" + err.Error())
+	}
+}
